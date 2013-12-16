0c9187bc806878c10ef38e87bc590acd2edf99b7
Stephen O'Reilly <stevey_oreilly@hotmail.com>
2013-12-16 10:02:42 +0100

    Added more http headers to support more browsers/tools.

--- src/com/hueemulator/server/Server.java
+++ src/com/hueemulator/server/Server.java
@@ -78,6 +78,15 @@ class MyHandler implements HttpHandler {
 
         OutputStream responseBody = exchange.getResponseBody();
         Headers responseHeaders = exchange.getResponseHeaders();
+        responseHeaders.set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+        responseHeaders.set("Pragma", "no-cache");
+        responseHeaders.set("Expires", "Mon, 1 Aug 2011 09:00:00 GMT");
+        responseHeaders.set("Connection", "close");  // Not sure if the server will actually close the connections by just setting the header
+        responseHeaders.set("Access-Control-Max-Age", "0");
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        responseHeaders.set("Access-Control-Allow-Credentials", "true");
+        responseHeaders.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+        responseHeaders.set("Access-Control-Allow-Headers", "Content-Type");
         responseHeaders.set("Content-Type", "application/json; charset=utf-8");
         exchange.sendResponseHeaders(200, 0);
         ObjectMapper mapper = new ObjectMapper();
